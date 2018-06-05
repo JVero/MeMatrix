66211bbfe2f30b2ee6e334efd45a6b8973b163ee
JVero <joev714@gmail.com>
2018-06-05 01:01:49 -0400

    clang-format

--- main.cpp
+++ main.cpp
@@ -2,27 +2,30 @@
  * This file is a proxy for a test-file.  I have not used any testing library yet
  */
 
-#include <iostream>
 #include "matrix.cpp"
+#include <iostream>
 
-void test_mat_init() {
-	auto mat = matrix<int>(4, 5);
+void test_mat_init()
+{
+    auto mat = matrix<int>(4, 5);
 }
 
-matrix<int> test_mat_add() {
-	auto m1 = matrix<int>(4, 5);
-	auto m2 = matrix<int>(4, 5);
-	return m1 + m2;
+matrix<int> test_mat_add()
+{
+    auto m1 = matrix<int>(4, 5);
+    auto m2 = matrix<int>(4, 5);
+    return m1 + m2;
 }
 
-int main() {
-	test_mat_add();
-	auto m = matrix<int>(4,4);
-	m(1, 0) += 4;
-	
-	auto n = m + m;
-	auto k = m - n;
-	std::cout << "\n";
-	std:: cout << m(1, 0) << std::endl;
-	return 0;
+int main()
+{
+    test_mat_add();
+    auto m = matrix<int>(4, 4);
+    m(1, 0) += 4;
+
+    auto n = m + m;
+    auto k = m - n;
+    std::cout << "\n";
+    std::cout << m(1, 0) << std::endl;
+    return 0;
 }
--- matrix.cpp
+++ matrix.cpp
@@ -1,45 +1,55 @@
 
 #include "matrix.hpp"
-#include<vector>
-#include<memory>
+#include <vector>
 
 // Default constructor
-template<typename T>
-matrix<T>::matrix(): r(3), c(3), std::vector<T>(9, 0) {};
+template <typename T>
+matrix<T>::matrix()
+    : r(3)
+    , c(3)
+    , std::vector<T>(9, 0){};
 
 // Blank initializer
-template<typename T>
-matrix<T>::matrix(int r, int c): r(r), c(c), values(std::vector<T>(r*c, 0)){};
+template <typename T>
+matrix<T>::matrix(int r, int c)
+    : r(r)
+    , c(c)
+    , values(std::vector<T>(r * c, 0)){};
 
 // Copy constructor
-template<typename T>
-matrix<T>::matrix(matrix<T> const&old): r(old.r), c(old.c), values(old.values) {};
+template <typename T>
+matrix<T>::matrix(matrix<T> const& old)
+    : r(old.r)
+    , c(old.c)
+    , values(old.values){};
 
 // Overload [] operator to return a "row object"
-template<typename T>
-T& matrix<T>::operator()(unsigned r, unsigned c) {
-	return this->values[r*c + c];
+template <typename T>
+T& matrix<T>::operator()(unsigned r, unsigned c)
+{
+    return this->values[r * c + c];
 }
 
-template<typename T>
-matrix<T> matrix<T>::operator+(matrix<T> right) const{
-	matrix<T> ret(right);
-	for (auto i = 0; i < this->r; ++i) {
-	  for (auto j = 0; j < this->c; ++j) {
-	    ret(i, j) += right(i, j);
-	  }
-	}
-	return ret;
-}
-
-template<typename T>
-matrix<T> matrix<T>::operator-(matrix<T> &right)const {
-        matrix<T> ret(right);
-        for (auto i = 0; i < this->r; ++i) {
-          for (auto j = 0; j < this->c; ++j) {
-            ret(i, j) -= right(i, j);
-          }
+template <typename T>
+matrix<T> matrix<T>::operator+(matrix<T> right) const
+{
+    matrix<T> ret(right);
+    for (auto i = 0; i < this->r; ++i) {
+        for (auto j = 0; j < this->c; ++j) {
+            ret(i, j) += right(i, j);
         }
-        return ret;
+    }
+    return ret;
 }
 
+template <typename T>
+matrix<T> matrix<T>::operator-(matrix<T>& right) const
+{
+    matrix<T> ret(right);
+    for (auto i = 0; i < this->r; ++i) {
+        for (auto j = 0; j < this->c; ++j) {
+            ret(i, j) -= right(i, j);
+        }
+    }
+    return ret;
+}
