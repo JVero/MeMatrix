d37bd634b85b1ae56764943cb9b4b599c9ec1ff0
Joseph <joev714@gmail.com>
2018-06-04 22:46:45 -0400

    Removed pointless 'using' statements

--- matrix.cpp
+++ matrix.cpp
@@ -1,17 +1,14 @@
 #include "matrix.hpp"
 #include <iostream>
 #include <vector>
-using std::cout;
-using std::endl;
-using std::vector;
 
 // empty matrix
 template<typename T>
 matrix<T>::matrix(int rows, int columns)
     : r(rows), c(columns),
-      values(vector<T>(rows*columns, 0)){
-  cout << "Vector initialized with " << rows << " rows and " << columns
-       << " columns." << endl;
+      values(std::vector<T>(rows*columns, 0)){
+      std::cout << "Vector initialized with " << rows << " rows and " << columns
+       << " columns." << std::endl;
 };
 
 template<typename T>
@@ -22,7 +19,7 @@ T matrix<T>::get(int row, int col) { return this->values[row*c+col]; }
 
 
 template<typename T>
-row<T> matrix<T>::operator[](int rw) const { return row<T>{rw, this->c,  &this->values};};
+row<T> matrix<T>::operator[](int rw)  { return row<T>{rw, this->c,  &this->values};};
 
 template<typename T>
 matrix<T> matrix<T>::operator+(const int &val) const {
@@ -45,7 +42,7 @@ matrix<T> matrix<T>::operator+(const matrix &right) const {
   // in addition to the values
   for (auto i = 0; i < this->r; ++i) {
     for (auto j = 0; j < this->c; ++j) {
-      *(ret[i][j]) = (*this)[i][j] + right[i][j];
+      ret[i][j] = this[i][j] + right[i][j];
     }
   }
   return ret;
